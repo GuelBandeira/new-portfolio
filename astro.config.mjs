5fb92895da2a271503d61ce321b62d92615cb333
GuelBandeira <bandeiraguel@gmail.com>
2025-11-08 09:16:39 -0300

    Enhance portfolio configuration and component performance
    - Optimized image loading in `ProjectCard.astro` and `Welcome.astro` by adding `loading="lazy"` and `decoding="async"` attributes.
    - Improved accessibility and performance with subtle hover transitions in `ProjectCard.astro`.
    - Updated layout and meta tags in `Layout.astro` for better SEO and user experience.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import icon from 'astro-icon';
+
 export default defineConfig({
-   site: 'https://guelbandeira.github.io/new-portfolio',
-   base: '/new-portfolio/',
-   output: 'static',
-   build: {
-      assets: '_astro'
-   },
+  site: 'https://guelbandeira.github.io/new-portfolio',
+  base: '/new-portfolio/',
+  output: 'static',
+
+  build: {
+     assets: '_astro'
+  },
+
+  integrations: [icon()],
 });
